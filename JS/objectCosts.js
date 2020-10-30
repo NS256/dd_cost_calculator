13c845484daaecfe2ccfb7952e2bf401d4fcab2f
Nathan <nls999@live.co.uk>
2020-10-30 21:24:47 +0000

    Added warnings and clear warnings button
    Added warnings and clear warnings button.
    Added functionality to set event length

--- JS/objectCosts.js
+++ JS/objectCosts.js
@@ -1,6 +1,7 @@
 var itemPriceList = [
     {
         "name": "XLR Leads",
+        "varName": "xlrLeads",
         "pricePerHour": 1.00,
         "pricePerDay": 12.00,
         "maxQuantity": 2,
@@ -8,16 +9,34 @@ var itemPriceList = [
     },
     {
         "name": "IEC Leads",
+        "varName": "iecLeads",
         "pricePerHour": 1.00,
         "pricePerDay": 13.00,
-        "maxQuantity": 2,
+        "maxQuantity": 3,
         "itemCategory": "box"
     },
     {
         "name": "Line Array Leads",
+        "varName": "lineArrayLeads",
         "pricePerHour": 1.00,
         "pricePerDay": 14.00,
         "maxQuantity": 1,
         "itemCategory": "box"
+    },
+    {
+        "name": "RCF HDL20a",
+        "varName": "rcf-hdl20a",
+        "pricePerHour": 7.00,
+        "pricePerDay": 21.50,
+        "maxQuantity": 16,
+        "itemCategory": "speaker"
+    },
+    {
+        "name": "Midas Pro 1",
+        "varName": "midasPro1",
+        "pricePerHour": 25.00,
+        "pricePerDay": 100,
+        "maxQuantity": 16,
+        "itemCategory": "mixingDesk"
     }
 ];
